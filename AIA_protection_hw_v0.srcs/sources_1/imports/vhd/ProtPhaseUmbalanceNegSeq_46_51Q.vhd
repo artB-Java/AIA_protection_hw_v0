2655073cc4393a294a7bf9fcfb2e0630715df06b
Arthur Biliato Javaroni <arthur.biliato@gmail.com>
2026-07-06 18:56:33 -0300

    51Q/46

--- AIA_protection_hw_v0.srcs/sources_1/imports/vhd/ProtPhaseUmbalanceNegSeq_46_51Q.vhd
+++ AIA_protection_hw_v0.srcs/sources_1/imports/vhd/ProtPhaseUmbalanceNegSeq_46_51Q.vhd
@@ -19,6 +19,7 @@ entity ProtPhaseUmbalanceNegSeq_46_51Q is
     -- Histerese em "contagens RMS" para evitar chatter (i_peakup - G_HYST).
     -- Ex.: se G_HYST=10, sai do temporizado quando RMS <= (peakup-10)
     G_HYST      : natural := 0;
+    G_TIME_WIDTH       : natural := 20;
     -- Larguras da LUT (RAM) usadas para a curva temporizada.
     G_ADDR_BITS : natural := 12; -- 2^12 = 4096 endereços (RMS 0..4095)
     G_DATA_BITS : natural := 20  -- tempo em ms, até ~1.048.575 ms
@@ -38,19 +39,19 @@ entity ProtPhaseUmbalanceNegSeq_46_51Q is
     i_seq2_valid        : in  std_logic; -- '1' quando RMS atual é válido (amostras espaçadas)
     i_seq2_pickup_e1    : in  std_logic_vector(11 downto 0); -- limiar de atuação (contagens RMS)
     i_seq2_pickup_e2    : in  std_logic_vector(11 downto 0); -- limiar de atuação (contagens RMS)
+    i_delay_e1_ms       : in unsigned(G_TIME_WIDTH-1 downto 0);
 
     --------------------------
     -- Interface RAM (LUT)
     --------------------------
-    o_ram_addr         : out std_logic_vector(G_ADDR_BITS-1 downto 0); -- endereço (RMS mapeado)
+    o_ram_addr         : out std_logic_vector(G_ADDR_BITS-1 downto 0);  -- endereço (RMS mapeado)
     o_ram_rd_req       : out std_logic;                                 -- pulso de leitura (1 ciclo)
     i_ram_data         : in  std_logic_vector(G_DATA_BITS-1 downto 0);  -- tempo-alvo em ms
 
     --------------------------
     -- Saídas de proteção / debug
     --------------------------
-    -- o_time_ms          : out std_logic_vector(G_DATA_BITS-1 downto 0);  -- contador de ms (satura)
-    -- o_start_trip_time  : out std_logic;
+    o_time_ms          : out std_logic_vector(G_DATA_BITS-1 downto 0);  -- contador de ms (satura)
     o_alarm_e1         : out std_logic;
     o_alarm_e2         : out std_logic;                        
     o_trip_46_e1       : out std_logic;
@@ -69,7 +70,7 @@ architecture rtl of ProtPhaseUmbalanceNegSeq_46_51Q is
   --------------------------------------------------------------------
   -- Tipos e estados
   --------------------------------------------------------------------
-  type t_state is (S_IDLE, S_MONITORING, S_TIME_WAIT_RD, S_TIME_ACTIVE, S_TRIPPED);
+  type t_state is (S_IDLE, S_MONITORING,S_E1_TIMING, S_TIME_WAIT_RD, S_TIME_ACTIVE, S_E2_TRIPPED, S_E1_TRIPPED);
 
   --------------------------------------------------------------------
   -- Sinais internos
@@ -85,22 +86,25 @@ architecture rtl of ProtPhaseUmbalanceNegSeq_46_51Q is
   signal peak_e1_u12                    : unsigned(11 downto 0);
   signal peak_e2_u12                    : unsigned(11 downto 0);
   signal hyst_u12                       : unsigned(11 downto 0);
-  --signal hyst_e2_u12                       : unsigned(11 downto 0);
-  signal low_thr_e1_u12                    : unsigned(11 downto 0);  -- (peak - G_HYST) saturado em 0
-  signal low_thr_e2_u12                    : unsigned(11 downto 0);
-  signal e1_above_peak, e1_below_low          : std_logic;
-  signal e2_above_peak, e2_below_low          : std_logic;
+
+  signal low_thr_e1_u12                 : unsigned(11 downto 0);  -- (peak - G_HYST) saturado em 0
+  signal low_thr_e2_u12                 : unsigned(11 downto 0);
+  signal e1_above_peak, e1_below_low    : std_logic;
+  signal e2_above_peak, e2_below_low    : std_logic;
 
   -- divisor de 1 ms
   signal ms_div_cnt                     : natural range 0 to C_MS_TICKS-1 := 0;
   signal ms_tick                        : std_logic := '0';
+  
 
   -- contador de tempo em ms (saturado)
-  signal time_ms_e1_reg                    : unsigned(G_DATA_BITS-1 downto 0) := (others => '0');
-  signal time_cnt_e1_en                    : std_logic := '0';
+  signal time_ms_reg          : unsigned(G_DATA_BITS-1 downto 0) := (others => '0');
+  signal time_cnt_en       : std_logic := '0';
+  signal e1_ms_cnt            : unsigned(G_TIME_WIDTH-1 downto 0) := (others => '0');
 
-  signal time_ms_e2_reg                    : unsigned(G_DATA_BITS-1 downto 0) := (others => '0');
-  signal time_cnt_e2_en                    : std_logic := '0';
+
+  -- signal time_ms_e2_reg                    : unsigned(G_DATA_BITS-1 downto 0) := (others => '0');
+  -- signal time_cnt_e2_en                    : std_logic := '0';
 
   -- tempo alvo em ms (lido da RAM)
   signal target_ms_reg                  : unsigned(G_DATA_BITS-1 downto 0) := (others => '0');
@@ -108,11 +112,9 @@ architecture rtl of ProtPhaseUmbalanceNegSeq_46_51Q is
   -- interface RAM
   signal ram_addr_reg                   : unsigned(G_ADDR_BITS-1 downto 0) := (others => '0');
   signal ram_rd_req_pulse               : std_logic := '0';
-  signal s_rd_req_d               		: std_logic := '0';
+  signal s_rd_req_d               		  : std_logic := '0';
   signal s_ram_data_valid               : std_logic := '0';
   
-
-
   -- saída de início de temporização (pulso)
   -- signal start_trip_e1_pulse_reg              : std_logic := '0';
   -- signal start_trip_e2_pulse_reg              : std_logic := '0';
@@ -121,23 +123,22 @@ architecture rtl of ProtPhaseUmbalanceNegSeq_46_51Q is
   signal alarm_e2_reg                      : std_logic := '0';
 
   -- trip latched
-  signal trip_reg                          : std_logic := '0';
   signal trip_e1_reg                       : std_logic := '0';
   signal trip_e2_reg                       : std_logic := '0';
 
 
   -- utilitários
-  function sat11_from_u12(x : unsigned(11 downto 0)) return unsigned is
-    -- Mapeia 12 bits -> 11 bits de endereço [0..2047] com saturação.
-    variable y : unsigned(10 downto 0);
-  begin
-    if x(11) = '1' then
-      y := (others => '1'); -- >= 2048 -> 2047
-    else
-      y := x(10 downto 0);
-    end if;
-    return resize(y, G_ADDR_BITS);
-  end function;
+  -- function sat11_from_u12(x : unsigned(11 downto 0)) return unsigned is
+  --   -- Mapeia 12 bits -> 11 bits de endereço [0..2047] com saturação.
+  --   variable y : unsigned(10 downto 0);
+  -- begin
+  --   if x(11) = '1' then
+  --     y := (others => '1'); -- >= 2048 -> 2047
+  --   else
+  --     y := x(10 downto 0);
+  --   end if;
+  --   return resize(y, G_ADDR_BITS);
+  -- end function;
 
 begin
   -----------------------------------------------------------------------------
@@ -189,14 +190,14 @@ begin
   begin
     if rising_edge(i_clk_100MHz) then
       if i_rst = '1' then
-        time_ms_e1_reg <= (others => '0');
+        time_ms_reg <= (others => '0');
       else
-        if time_cnt_e1_en = '0' then
-          time_ms_e1_reg <= (others => '0');
+        if time_cnt_en = '0' then
+          time_ms_reg <= (others => '0');
         else
           if ms_tick = '1' then
-            if time_ms_e1_reg /= (time_ms_e1_reg'range => '1') then
-              time_ms_e1_reg <= time_ms_e1_reg + 1;
+            if time_ms_reg /= (time_ms_reg'range => '1') then
+              time_ms_reg <= time_ms_reg + 1;
             end if;
           end if;
         end if;
@@ -231,22 +232,20 @@ begin
     if rising_edge(i_clk_100MHz) then
       if i_rst = '1' then
         state                <= S_IDLE;
-        trip_reg             <= '0';
+        trip_e1_reg          <= '0';
+        trip_e2_reg          <= '0';
         target_ms_reg        <= (others => '0');
         ram_addr_reg         <= (others => '0');
         ram_rd_req_pulse     <= '0';
 		    s_rd_req_d      	   <= '0';
 		    s_ram_data_valid     <= '0';		
-        --start_trip_pulse_reg <= '0';
         alarm_e1_reg         <= '0'; 
         alarm_e2_reg         <= '0';
-        time_cnt_e1_en       <= '0';
+        time_cnt_en          <= '0';
       else
         state <= state_nxt;
-
         -- defaults a cada ciclo
         ram_rd_req_pulse     <= '0';
-        start_trip_pulse_reg <= '0';
         alarm_e1_reg         <= '0'; 
         alarm_e2_reg         <= '0';
 		
@@ -257,23 +256,26 @@ begin
         -- ações por estado
         case state is
           when S_IDLE =>
-            trip_reg    <= '0';
+            trip_e1_reg          <= '0';
+            trip_e2_reg          <= '0';
             time_cnt_en <= '0';
 
           when S_MONITORING =>
             time_cnt_en <= '0';
             -- Verifica apenas nos pulsos válidos de RMS
-            if i_rms_51_51N_valid = '1' then
-              if above_peak = '1' then
-                -- Endereço = RMS mapeado com saturação (11b)
-                ram_addr_reg     <= sat11_from_u12(rms_u12);
-                ram_rd_req_pulse <= '1';           -- requisita leitura da LUT
-                time_cnt_en      <= '1';           -- inicia contagem de ms já neste estado
-                --start_trip_pulse_reg <= '1';       -- pulso de início de temporização
-                alarm_e1_reg         <= '1'; 
-                --alarm_e2_reg         <= '0';
+            if i_seq2_valid = '1' then
+              if e2_above_peak = '1' then
+                ram_addr_reg       <= seq_abs_u12;
+                ram_rd_req_pulse   <= '1';           -- requisita leitura da LUT
+                time_cnt_en     <= '1';           -- inicia contagem de ms já neste estado
+                alarm_e2_reg <= '1';
+              elsif e1_above_peak = '1' then
+                alarm_e1_reg <= '1';
               end if;
             end if;
+
+          when S_E1_TIMING =>
+            
 
           when S_TIME_WAIT_RD =>
             time_cnt_en <= '1'; -- contador ativo enquanto aguardamos o dado da RAM
@@ -281,15 +283,14 @@ begin
             if s_ram_data_valid = '1' then
               target_ms_reg <= unsigned(i_ram_data);
             end if;
-
             -- Atualizações assíncronas por novos RMS válidos:
-            if i_rms_51_51N_valid = '1' then
-              if below_low = '1' then
+            if i_seq2_valid = '1' then
+              if e2_below_low = '1' then
                 -- aborta temporização e retorna ao monitoramento
                 time_cnt_en <= '0';
               else
                 -- pode atualizar o endereço (mantém LUT "on-the-fly")
-                ram_addr_reg     <= sat11_from_u12(rms_u12);
+                ram_addr_reg     <= seq_abs_u12;
                 ram_rd_req_pulse <= '1';
               end if;
             end if;
@@ -297,11 +298,11 @@ begin
           when S_TIME_ACTIVE =>
             time_cnt_en <= '1';
             -- Leitura contínua da LUT em cada novo RMS válido (mantém curva atualizada)
-            if i_rms_51_51N_valid = '1' then 
-              if below_low = '1' then
+            if i_seq2_valid = '1' then 
+              if e2_below_low = '1' then
                 time_cnt_en <= '0'; -- será efetivado na próxima transição
               else
-                ram_addr_reg     <= sat11_from_u12(rms_u12);
+                ram_addr_reg     <= seq_abs_u12;
                 ram_rd_req_pulse <= '1';
               end if;
             end if;
@@ -313,24 +314,20 @@ begin
 
             -- Disparo quando time_ms >= target_ms
             if time_ms_reg >= target_ms_reg and target_ms_reg /= 0 then
-              trip_reg <= '1';
+              trip_e2_reg <= '1';
             end if;
 
-          when S_TRIPPED =>
+          when S_E2_TRIPPED =>
             time_cnt_en <= '0';
             -- trip_reg fica latched até reset ou novo start
 			
-			-- Antes ficava latched, mas acho melhor abaixar se cair do bellow low e a proteção de 86 mantentém ativado caso configurada
-			if i_rms_51_51N_valid = '1' then
-				if below_low = '1' then
-					time_cnt_en <= '0'; -- será efetivado na próxima transição
-					trip_reg    <= '0';
-				end if;
+			      -- Antes ficava latched, mas acho melhor abaixar se cair do bellow low e a proteção de 86 mantentém ativado caso configurada
+            if i_seq2_valid = '1' then
+              if e2_below_low = '1' then
+                time_cnt_en <= '0'; -- será efetivado na próxima transição
+                trip_e2_reg    <= '0';
+              end if;
             end if;
-			
-	
-            null;
-
           when others =>
             null;
         end case;
@@ -341,49 +338,70 @@ begin
   -----------------------------------------------------------------------------
   -- Próximo estado (combinacional)
   -----------------------------------------------------------------------------
-  p_fsm_comb : process(state, i_start_51_51N, start_pulse, i_rms_51_51N_valid,
-                       above_peak, below_low, s_ram_data_valid, trip_reg)
+  p_fsm_comb : process(state, i_start, start_pulse, i_seq2_valid, s_ram_data_valid,
+                       e1_above_peak, e1_below_low, trip_e1_reg,
+                       e2_above_peak, e2_below_low, trip_e2_reg)
   begin
     state_nxt <= state;
 
     case state is
       when S_IDLE =>
-        if (i_start_51_51N = '1') or (start_pulse = '1') then
+        if (i_start = '1') or (start_pulse = '1') then
           state_nxt <= S_MONITORING;
         end if;
 
       when S_MONITORING =>
-        if (i_rms_51_51N_valid = '1') and (above_peak = '1') then
+        if (i_seq2_valid = '1') and (e2_above_peak = '1') then
           state_nxt <= S_TIME_WAIT_RD;
+        elsif (i_seq2_valid = '1') and (e1_above_peak = '1') then
+          if i_delay_e1_ms = 0 then
+                state_nxt <= S_TRIPPED_E1;
+              else
+                state_nxt <= S_E1_TIMING;
+              end if;
         end if;
 
+      when S_E1_TIMING =>
+        if (i_seq2_valid = '1') and (e2_above_peak = '1') then
+          state_nxt <= S_TIME_WAIT_RD;
+        elsif (i_seq2_valid = '1') and (e1_below_low = '1') then
+          state_nxt <= S_MONITORING;
+        elsif trip_e1_reg = '1' then
+          state_nxt <= S_E1_TRIPPED;
+        else
+          state_nxt <= S_E1_TIMING;
+        end if;
+      
       when S_TIME_WAIT_RD =>
-        if (i_rms_51_51N_valid = '1') and (below_low = '1') then
+        if (i_seq2_valid = '1') and (e2_above_peak = '1') then
           state_nxt <= S_MONITORING;
         elsif (s_ram_data_valid = '1') then
           state_nxt <= S_TIME_ACTIVE;
         end if;
 
       when S_TIME_ACTIVE =>
-        if trip_reg = '1' then
-          state_nxt <= S_TRIPPED;
-        elsif (i_rms_51_51N_valid = '1') and (below_low = '1') then
+        if trip_e2_reg = '1' then
+          state_nxt <= S_E2_TRIPPED;
+        elsif (i_seq2_valid = '1') and (e2_above_peak = '1') then
           state_nxt <= S_MONITORING;
         else
           -- permanece temporizando enquanto acima do limiar inferior
           state_nxt <= S_TIME_ACTIVE;
         end if;
 
-      when S_TRIPPED =>
+      when S_E1_TRIPPED =>
+        
+
+      when S_E2_TRIPPED =>
         -- libera com reset ou novo start
         if start_pulse = '1' then
           state_nxt <= S_MONITORING;
         end if;
 		
-		-- Antes deixava em latched, mas agora se cair do bellow low ele vai para monitoring, vou deixar para a funcaoa 86 segurar caso trip caso configurada.
-		if (i_rms_51_51N_valid = '1') and (below_low = '1') then
-			state_nxt <= S_MONITORING;
-		end if;
+        -- Antes deixava em latched, mas agora se cair do bellow low ele vai para monitoring, vou deixar para a funcaoa 86 segurar caso trip caso configurada.
+        if (i_seq2_valid = '1') and (e2_below_low = '1') then
+          state_nxt <= S_MONITORING;
+        end if;
 
       when others =>
         state_nxt <= S_IDLE;
@@ -393,9 +411,11 @@ begin
   -----------------------------------------------------------------------------
   -- Saídas
   -----------------------------------------------------------------------------
-  o_trip_51_51N     <= trip_reg;
+  o_trip_46          <= trip_e1_reg or trip_e2_reg;
+  o_trip_46_e1       <= trip_e1_reg;
+  o_trip_46_e2       <= trip_e2_reg;
   o_time_ms         <= std_logic_vector(time_ms_reg);
-  --o_start_trip_time <= start_trip_pulse_reg;
+  
   o_alarm_e1 <= alarm_e1_reg;          
   o_alarm_e2 <= alarm_e2_reg;         
 
