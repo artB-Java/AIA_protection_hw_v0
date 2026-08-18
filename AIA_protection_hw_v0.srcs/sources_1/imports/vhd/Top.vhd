24af971854e8700dc7b2571cae59ccc0751e8b46
Arthur Biliato Javaroni <arthur.biliato@gmail.com>
2026-08-18 09:35:38 -0300

    configurações inicias do teste ddrless

--- AIA_protection_hw_v0.srcs/sources_1/imports/vhd/Top.vhd
+++ AIA_protection_hw_v0.srcs/sources_1/imports/vhd/Top.vhd
@@ -69,23 +69,23 @@ entity Top is
     ------------------
     -- Fixed PS pins
     ------------------
-    DDR_addr          : inout std_logic_vector (14 downto 0);
-    DDR_ba            : inout std_logic_vector (2 downto 0);
-    DDR_cas_n         : inout std_logic;
-    DDR_ck_n          : inout std_logic;
-    DDR_ck_p          : inout std_logic;
-    DDR_cke           : inout std_logic;
-    DDR_cs_n          : inout std_logic;
-    DDR_dm            : inout std_logic_vector (3 downto 0);
-    DDR_dq            : inout std_logic_vector (31 downto 0);
-    DDR_dqs_n         : inout std_logic_vector (3 downto 0);
-    DDR_dqs_p         : inout std_logic_vector (3 downto 0);
-    DDR_odt           : inout std_logic;
-    DDR_ras_n         : inout std_logic;
-    DDR_reset_n       : inout std_logic;
-    DDR_we_n          : inout std_logic;
-    FIXED_IO_ddr_vrn  : inout std_logic;
-    FIXED_IO_ddr_vrp  : inout std_logic;
+    -- DDR_addr          : inout std_logic_vector (14 downto 0);
+    -- DDR_ba            : inout std_logic_vector (2 downto 0);
+    -- DDR_cas_n         : inout std_logic;
+    -- DDR_ck_n          : inout std_logic;
+    -- DDR_ck_p          : inout std_logic;
+    -- DDR_cke           : inout std_logic;
+    -- DDR_cs_n          : inout std_logic;
+    -- DDR_dm            : inout std_logic_vector (3 downto 0);
+    -- DDR_dq            : inout std_logic_vector (31 downto 0);
+    -- DDR_dqs_n         : inout std_logic_vector (3 downto 0);
+    -- DDR_dqs_p         : inout std_logic_vector (3 downto 0);
+    -- DDR_odt           : inout std_logic;
+    -- DDR_ras_n         : inout std_logic;
+    -- DDR_reset_n       : inout std_logic;
+    -- DDR_we_n          : inout std_logic;
+    -- FIXED_IO_ddr_vrn  : inout std_logic;
+    -- FIXED_IO_ddr_vrp  : inout std_logic;
     FIXED_IO_mio      : inout std_logic_vector (53 downto 0);
     FIXED_IO_ps_clk   : inout std_logic;
     FIXED_IO_ps_porb  : inout std_logic;
@@ -288,39 +288,55 @@ architecture Behavioral of Top is
   -- =========================	
   component GOD_wrapper is
     port (
-      Clk                : in std_logic;
-      DDR_addr           : inout std_logic_vector (14 downto 0);
-      DDR_ba             : inout std_logic_vector (2 downto 0);
-      DDR_cas_n          : inout std_logic;
-      DDR_ck_n           : inout std_logic;
-      DDR_ck_p           : inout std_logic;
-      DDR_cke            : inout std_logic;
-      DDR_cs_n           : inout std_logic;
-      DDR_dm             : inout std_logic_vector (3 downto 0);
-      DDR_dq             : inout std_logic_vector (31 downto 0);
-      DDR_dqs_n          : inout std_logic_vector (3 downto 0);
-      DDR_dqs_p          : inout std_logic_vector (3 downto 0);
-      DDR_odt            : inout std_logic;
-      DDR_ras_n          : inout std_logic;
-      DDR_reset_n        : inout std_logic;
-      DDR_we_n           : inout std_logic;
-      FIXED_IO_ddr_vrn   : inout std_logic;
-      FIXED_IO_ddr_vrp   : inout std_logic;
-      FIXED_IO_mio       : inout std_logic_vector (53 downto 0);
-      FIXED_IO_ps_clk    : inout std_logic;
-      FIXED_IO_ps_porb   : inout std_logic;
-      FIXED_IO_ps_srstb  : inout std_logic;
-      PS_EMIO_tri_io     : inout std_logic_vector (4 downto 0);
-      PS_UART0_rxd       : in std_logic;
-      PS_UART0_txd       : out std_logic;
-      i2c0_scl_io        : inout std_logic;
-      i2c0_sda_io        : inout std_logic;
-      i_readdata_tri_i   : in std_logic_vector (31 downto 0);
-      o_address_tri_o    : out std_logic_vector (7 downto 0);
-      o_read_tri_o       : out std_logic_vector (0 to 0);
-      o_write_data_tri_o : out std_logic_vector (31 downto 0);
-      o_write_tri_o      : out std_logic_vector (0 to 0);
-      reset_rtl_0        : in std_logic
+        Clk : in STD_LOGIC;
+        FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+        FIXED_IO_ps_clk : inout STD_LOGIC;
+        FIXED_IO_ps_porb : inout STD_LOGIC;
+        FIXED_IO_ps_srstb : inout STD_LOGIC;
+        PS_EMIO_tri_io : inout STD_LOGIC_VECTOR ( 4 downto 0 );
+        PS_UART0_rxd : in STD_LOGIC;
+        PS_UART0_txd : out STD_LOGIC;
+        i2c0_scl_io : inout STD_LOGIC;
+        i2c0_sda_io : inout STD_LOGIC;
+        i_readdata_tri_i : in STD_LOGIC_VECTOR ( 31 downto 0 );
+        o_address_tri_o : out STD_LOGIC_VECTOR ( 7 downto 0 );
+        o_read_tri_o : out STD_LOGIC_VECTOR ( 0 to 0 );
+        o_write_data_tri_o : out STD_LOGIC_VECTOR ( 31 downto 0 );
+        o_write_tri_o : out STD_LOGIC_VECTOR ( 0 to 0 );
+        reset_rtl_0 : in STD_LOGIC
+      -- Clk                : in std_logic;
+      -- DDR_addr           : inout std_logic_vector (14 downto 0);
+      -- DDR_ba             : inout std_logic_vector (2 downto 0);
+      -- DDR_cas_n          : inout std_logic;
+      -- DDR_ck_n           : inout std_logic;
+      -- DDR_ck_p           : inout std_logic;
+      -- DDR_cke            : inout std_logic;
+      -- DDR_cs_n           : inout std_logic;
+      -- DDR_dm             : inout std_logic_vector (3 downto 0);
+      -- DDR_dq             : inout std_logic_vector (31 downto 0);
+      -- DDR_dqs_n          : inout std_logic_vector (3 downto 0);
+      -- DDR_dqs_p          : inout std_logic_vector (3 downto 0);
+      -- DDR_odt            : inout std_logic;
+      -- DDR_ras_n          : inout std_logic;
+      -- DDR_reset_n        : inout std_logic;
+      -- DDR_we_n           : inout std_logic;
+      -- FIXED_IO_ddr_vrn   : inout std_logic;
+      -- FIXED_IO_ddr_vrp   : inout std_logic;
+      -- FIXED_IO_mio       : inout std_logic_vector (53 downto 0);
+      -- FIXED_IO_ps_clk    : inout std_logic;
+      -- FIXED_IO_ps_porb   : inout std_logic;
+      -- FIXED_IO_ps_srstb  : inout std_logic;
+      -- PS_EMIO_tri_io     : inout std_logic_vector (4 downto 0);
+      -- PS_UART0_rxd       : in std_logic;
+      -- PS_UART0_txd       : out std_logic;
+      -- i2c0_scl_io        : inout std_logic;
+      -- i2c0_sda_io        : inout std_logic;
+      -- i_readdata_tri_i   : in std_logic_vector (31 downto 0);
+      -- o_address_tri_o    : out std_logic_vector (7 downto 0);
+      -- o_read_tri_o       : out std_logic_vector (0 to 0);
+      -- o_write_data_tri_o : out std_logic_vector (31 downto 0);
+      -- o_write_tri_o      : out std_logic_vector (0 to 0);
+      -- reset_rtl_0        : in std_logic
     );
   end component;
 
@@ -3413,23 +3429,23 @@ begin
     o_write_data_tri_o => s_o_write_data_tri_o,
     o_read_tri_o       => s_o_read_tri_o,
     i_readdata_tri_i   => s_i_readdata_tri_i,
-    DDR_addr           => DDR_addr,
-    DDR_ba             => DDR_ba,
-    DDR_cas_n          => DDR_cas_n,
-    DDR_ck_n           => DDR_ck_n,
-    DDR_ck_p           => DDR_ck_p,
-    DDR_cke            => DDR_cke,
-    DDR_cs_n           => DDR_cs_n,
-    DDR_dm             => DDR_dm,
-    DDR_dq             => DDR_dq,
-    DDR_dqs_n          => DDR_dqs_n,
-    DDR_dqs_p          => DDR_dqs_p,
-    DDR_odt            => DDR_odt,
-    DDR_ras_n          => DDR_ras_n,
-    DDR_reset_n        => DDR_reset_n,
-    DDR_we_n           => DDR_we_n,
-    FIXED_IO_ddr_vrn   => FIXED_IO_ddr_vrn,
-    FIXED_IO_ddr_vrp   => FIXED_IO_ddr_vrp,
+    -- DDR_addr           => DDR_addr,
+    -- DDR_ba             => DDR_ba,
+    -- DDR_cas_n          => DDR_cas_n,
+    -- DDR_ck_n           => DDR_ck_n,
+    -- DDR_ck_p           => DDR_ck_p,
+    -- DDR_cke            => DDR_cke,
+    -- DDR_cs_n           => DDR_cs_n,
+    -- DDR_dm             => DDR_dm,
+    -- DDR_dq             => DDR_dq,
+    -- DDR_dqs_n          => DDR_dqs_n,
+    -- DDR_dqs_p          => DDR_dqs_p,
+    -- DDR_odt            => DDR_odt,
+    -- DDR_ras_n          => DDR_ras_n,
+    -- DDR_reset_n        => DDR_reset_n,
+    -- DDR_we_n           => DDR_we_n,
+    -- FIXED_IO_ddr_vrn   => FIXED_IO_ddr_vrn,
+    -- FIXED_IO_ddr_vrp   => FIXED_IO_ddr_vrp,
     FIXED_IO_mio       => FIXED_IO_mio,
     FIXED_IO_ps_clk    => FIXED_IO_ps_clk,
     FIXED_IO_ps_porb   => FIXED_IO_ps_porb,
